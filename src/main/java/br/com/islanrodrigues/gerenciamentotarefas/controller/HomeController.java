ab200f284dc513f0768d57e00c4b9157c31b737d
Islan Rodrigues <jose-islan52@hotmail.com>
2019-07-03 20:19:08 -0300

    Criação da controller e view 'Home'

---
+++ src/main/java/br/com/islanrodrigues/gerenciamentotarefas/controller/HomeController.java
@@ -0,0 +1,16 @@
+package br.com.islanrodrigues.gerenciamentotarefas.controller;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() { 		
+		return "home/home";
+	}
+
+}
