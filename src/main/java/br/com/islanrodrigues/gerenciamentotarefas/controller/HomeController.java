692ea47a03def0d3569b2e03b97bfaae69d1ea47
Islan Rodrigues <jose-islan52@hotmail.com>
2019-07-03 23:46:15 -0300

    Implementando e utilizando Thymeleaf - comunicação entre a HomeController e a sua view

--- src/main/java/br/com/islanrodrigues/gerenciamentotarefas/controller/HomeController.java
+++ src/main/java/br/com/islanrodrigues/gerenciamentotarefas/controller/HomeController.java
@@ -1,16 +1,21 @@
 package br.com.islanrodrigues.gerenciamentotarefas.controller;
 
 import org.springframework.stereotype.Controller;
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/")
-	public String home() { 		
-		return "home/home";
+	public ModelAndView home() {
+		ModelAndView model = new ModelAndView();
+		model.setViewName("home/home");
+		model.addObject("mensagem", "Mensagem da Controller 'Home'");
+		
+		return model;
 	}
 
 }
