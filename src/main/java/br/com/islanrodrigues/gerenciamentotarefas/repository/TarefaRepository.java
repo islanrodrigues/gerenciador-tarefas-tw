61ea247b5d6f3f4b1b94c8d5e32c8faf614aeb39
Islan Rodrigues <jose-islan52@hotmail.com>
2019-07-10 18:03:18 -0300

    Implementando um método de consulta personalizado de tarefas via Spring Data JPA

--- src/main/java/br/com/islanrodrigues/gerenciamentotarefas/repository/TarefaRepository.java
+++ src/main/java/br/com/islanrodrigues/gerenciamentotarefas/repository/TarefaRepository.java
@@ -1,10 +1,17 @@
 package br.com.islanrodrigues.gerenciamentotarefas.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.islanrodrigues.gerenciamentotarefas.model.Tarefa;
 
 
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+	
+	@Query("SELECT t FROM Tarefa t WHERE t.usuario.email = :emailUsuario")
+	List<Tarefa> buscarTarefasPorUsuario(@Param("emailUsuario") String email);
 
 }
